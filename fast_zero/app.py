8661d6ea737df55a5801e6aa20123b277856b535
guilopes15 <guilherme-lopes58@hotmail.com>
2024-07-10 19:39:32 -0300

    Criando endpoint /ex que retorna um html e testando

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fast_zero.schemas import Message
 
@@ -10,3 +11,16 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Ola mundo'}
+
+
+@app.get('/ex', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def ola_mundo():
+    return """
+    <html>
+      <head>
+        <title>Olá Mundo</title>
+      </head>
+      <body>
+        <h1>Olá Mundo</h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,21 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
     response = client.get('/')  # Act (ação)
     assert response.status_code == HTTPStatus.OK  # Assert (afirmação)
     assert response.json() == {'message': 'Ola mundo'}
+
+
+def test_ola_mundo_deve_retornar_ok_e_htmlresponse():
+    client = TestClient(app)
+    response = client.get('/ex')
+
+    html = """
+    <html>
+      <head>
+        <title>Olá Mundo</title>
+      </head>
+      <body>
+        <h1>Olá Mundo</h1>
+      </body>
+    </html>"""
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == html
