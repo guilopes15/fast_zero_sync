3f0282c6ced4bc8bcf1e1e6cef5204fa96ac5118
guilopes15 <guilherme-lopes58@hotmail.com>
2024-07-22 12:05:31 -0300

    criando a coluna updated_at na tabela users e aplicando a migracao

--- migrations/env.py
+++ migrations/env.py
@@ -47,6 +47,8 @@ def run_migrations_offline() -> None:
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
+        compare_type=True,
+        render_as_batch=True,
         dialect_opts={"paramstyle": "named"},
     )
 
@@ -69,7 +71,10 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection, 
+            target_metadata=target_metadata, 
+            compare_type=True,
+            render_as_batch=True,
         )
 
         with context.begin_transaction():
