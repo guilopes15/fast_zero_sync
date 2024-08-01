e9dddcc8dfc1f879817d42dd9387ac9e0bd3c6fa
guilherme <guilherme-lopes58@hotmail.com>
2024-07-31 23:57:15 -0300

    Implementando endpoints de todos

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, ConfigDict, EmailStr
 
+from fast_zero.models import TodoState
+
 
 class Message(BaseModel):
     message: str
@@ -26,3 +28,23 @@ class Token(BaseModel):
     access_token: str
     # token_type é como o token deve ser usado
     token_type: str  # deve ser passado no header do post
+
+
+class TodoSchema(BaseModel):
+    title: str
+    description: str
+    state: TodoState
+
+
+class TodoPublic(TodoSchema):
+    id: int
+
+
+class TodoList(BaseModel):
+    todos: list[TodoPublic]
+
+
+class TodoUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    state: TodoState | None = None
